9540dd0878b251819d88db59ae3a0ddacb0892f4
w3aran <w3aran@users.noreply.github.com>
2015-05-18 10:13:04 -0400

    dependency issue

--- install_virtualbox_on_debian_7_and_8_64bit.sh
+++ install_virtualbox_on_debian_7_and_8_64bit.sh
@@ -4,6 +4,7 @@ PACKAGE_FILE_NAME=virtualbox-4.3_4.3.28-100309~Debian~wheezy_amd64.deb
 PACKAGE_DOWNLOAD_LINK=http://download.virtualbox.org/virtualbox/4.3.28/$PACKAGE_FILE_NAME
 
 wget $PACKAGE_DOWNLOAD_LINK
+sudo apt-get install -f
 sudo dpkg -i $PACKAGE_FILE_NAME
 sudo apt-get install -f
 rm $PACKAGE_FILE_NAME
