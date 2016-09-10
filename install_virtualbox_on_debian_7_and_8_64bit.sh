2e2286401a5a19e95250e6523e26f0db45aec502
w3aran <w3aran@users.noreply.github.com>
2016-09-10 10:40:56 -0400

    Upgraded from 4.3.28 to 5.1.4

--- install_virtualbox_on_debian_7_and_8_64bit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-PACKAGE_FILE_NAME=virtualbox-4.3_4.3.28-100309~Debian~wheezy_amd64.deb
-PACKAGE_DOWNLOAD_LINK=http://download.virtualbox.org/virtualbox/4.3.28/$PACKAGE_FILE_NAME
-
-wget $PACKAGE_DOWNLOAD_LINK
-sudo dpkg -i $PACKAGE_FILE_NAME
-sudo apt-get -f install
-rm $PACKAGE_FILE_NAME
---
+++ install_virtualbox_on_debian_8_64bit.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+PACKAGE_VERSION=5.1.4
+PACKAGE_FILE_NAME=virtualbox-5.1_5.1.4-110228~Debian~jessie_amd64.deb
+PACKAGE_DOWNLOAD_LINK=http://download.virtualbox.org/virtualbox/$PACKAGE_VERSION/$PACKAGE_FILE_NAME
+
+wget $PACKAGE_DOWNLOAD_LINK
+sudo dpkg -i $PACKAGE_FILE_NAME
+sudo apt-get -f install
+rm $PACKAGE_FILE_NAME
