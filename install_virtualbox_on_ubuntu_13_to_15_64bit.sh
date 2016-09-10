8fe03b6e854388d8eb34e18df7dcaab2d984c72e
w3aran <w3aran@users.noreply.github.com>
2016-09-10 10:45:28 -0400

    Upgraded from 4.3.28 to 5.1.4

--- install_virtualbox_on_ubuntu_13_to_15_64bit.sh
+++ install_virtualbox_on_ubuntu_13_to_15_64bit.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-PACKAGE_FILE_NAME=virtualbox-4.3_4.3.28-100309~Ubuntu~raring_amd64.deb
-PACKAGE_DOWNLOAD_LINK=http://download.virtualbox.org/virtualbox/4.3.28/$PACKAGE_FILE_NAME
+PACKAGE_VERSION=5.1.4
+PACKAGE_FILE_NAME=virtualbox-5.1_5.1.4-110228~Ubuntu~trusty_amd64.deb
+PACKAGE_DOWNLOAD_LINK=http://download.virtualbox.org/virtualbox/$PACKAGE_VERSION/$PACKAGE_FILE_NAME
 
 wget $PACKAGE_DOWNLOAD_LINK
 sudo dpkg -i $PACKAGE_FILE_NAME
